6a0f0afddb1cbf3950e737d893e9c9239d16b4de
demonizze <artem@securityarea.com>
2021-03-28 18:49:19 +0300

    создание нового проекта

---
+++ Database Factory/Database Factory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Database_Factory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            Console.WriteLine("Welcome to Database Factory!");
+
+        }
+    }
+}
